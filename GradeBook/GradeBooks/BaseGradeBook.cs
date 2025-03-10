989a855eae4e3203a7228fc70b87a7302ccbf2de
AranV1 <dominik.szczurek@microsoft.wsei.edu.pl>
2025-03-10 12:16:47 +0100

    GPA_Task3

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -111,20 +111,26 @@ namespace GradeBook.GradeBooks
 
         public virtual double GetGPA(char letterGrade, StudentType studentType)
         {
+            double gpa = 0;
+            if ((studentType == StudentType.Honors || studentType == StudentType.DualEnrolled) && IsWeighted==true)
+            {
+                gpa+=1;
+            }
+
             switch (letterGrade)
             {
                 case 'A':
-                    return 4;
+                    return gpa + 4;
                 case 'B':
-                    return 3;
+                    return gpa + 3;
                 case 'C':
-                    return 2;
+                    return gpa + 2;
                 case 'D':
-                    return 1;
+                    return gpa + 1;
                 case 'F':
-                    return 0;
+                    return gpa;
             }
-            return 0;
+            return gpa;
         }
 
         public virtual void CalculateStatistics()
